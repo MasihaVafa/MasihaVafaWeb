48259033ece8e5e1167d4df6c15d66e82a0c3dbf
unknown <zahedivafa7@gmail.com>
2020-04-15 20:20:09 +0430

    Updates

---
+++ precache-manifest.4068a14f5789bcc9d1dfe9be99e13a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c97cd8a7f564b03b70953bad94684a4",
+    "url": "/MasihaVafaWeb/index.html"
+  },
+  {
+    "revision": "04d546765813c8bc0797",
+    "url": "/MasihaVafaWeb/static/css/2.eb6c4891.chunk.css"
+  },
+  {
+    "revision": "22cd028c53deed2ddf20",
+    "url": "/MasihaVafaWeb/static/css/main.396baab9.chunk.css"
+  },
+  {
+    "revision": "04d546765813c8bc0797",
+    "url": "/MasihaVafaWeb/static/js/2.53d23eba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MasihaVafaWeb/static/js/2.53d23eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cd028c53deed2ddf20",
+    "url": "/MasihaVafaWeb/static/js/main.f900c323.chunk.js"
+  },
+  {
+    "revision": "03690b221021ad79cff9",
+    "url": "/MasihaVafaWeb/static/js/runtime-main.d0fdc1aa.js"
+  }
+]);
